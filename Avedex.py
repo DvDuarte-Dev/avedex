19d6c86c9a64d21e956444bd61a9284ab4535c7e
DvDuarte-Dev <dvduarte002@hotmail.com>
2026-08-03 11:38:01 -0300

    Refatorar o menu

--- Avedex.py
+++ Avedex.py
@@ -290,6 +290,30 @@ def mostrar_sobre():
     print("O projeto evolui durante a disciplina de Boas Práticas.")
     print("Futuramente teremos busca, comparação e testes.")
 
+def processar_opcao(opcao, catalogo):
+    if opcao == "1":
+        listar_aves(catalogo)
+
+    elif opcao == "2":
+        tela_busca(catalogo)
+
+    elif opcao == "3":
+        selecionar_ave_por_id(catalogo)
+
+    elif opcao == "4":
+        tela_comparacao(catalogo)
+
+    elif opcao == "5":
+        mostrar_sobre()
+
+    elif opcao == "0":
+        print("Encerrando a AveDex.")
+
+    else:
+        print(
+            "Opção inválida. "
+            "Digite apenas 0, 1, 2, 3, 4 ou 5."
+        )
 
 catalogo_aves = [
     {
@@ -465,31 +489,14 @@ while opcao_menu != "0":
     ).strip()
 
     print()
-    
-    if opcao_menu == "1":
-        listar_aves(catalogo_aves)
 
-    elif opcao_menu == "2":
-        tela_busca(catalogo_aves)
+    processar_opcao(
+        opcao_menu,
+        catalogo_aves
+    )
 
-    elif opcao_menu == "3":
-        selecionar_ave_por_id(catalogo_aves)
-
-    elif opcao_menu == "4":
-        tela_comparacao(catalogo_aves)
-
-    elif opcao_menu == "5":
-        mostrar_sobre()
-
-    elif opcao_menu == "0":
-        print("Encerrando a AveDex.")
+    if opcao_menu == "0":
         print(f"Até logo, {nome_usuario}!")
-    
-    else:
-        print(
-            "Opção inválida. "
-            "Digite apenas 0, 1, 2, 3, 4 ou 5."
-        )
 
     if opcao_menu != "0":
         pausar()
